fe7cef7e5f5269f745aade70e3a7d83316f2bb06
dimka <dmitry_6614@mail.ru>
2024-03-13 00:32:09 +0300

    difficultpath

--- 1cod/hello.rb
+++ 1cod/hello.rb
@@ -1,3 +1,5 @@
 puts 'hello, world'
-puts 'goodbuy, world
-'
+puts 'goodbuy, world'
+puts 'my path will be difficult, but Im sure I can handle it'
+puts 'if I cant cope, Ill come here and remember where I started'
+
