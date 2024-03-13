da48eece2f301c07533a3e66080819488c210614
dimka <dmitry_6614@mail.ru>
2024-03-14 00:05:17 +0300

    ball

---
+++ 1cod/data-types.rb
@@ -0,0 +1,72 @@
+#integer
+
+a = 2
+b = 34
+puts a
+puts b
+
+#float
+
+g = 1.5
+o = 2.4
+puts g
+puts o
+
+puts a + g
+
+puts b - o
+
+#string
+
+m = 'dima'
+n = 'ilya'
+
+puts m + n
+
+#array
+
+c = ['under', 34, 'simvol']
+v = [2, 'sun', 156 ]
+
+puts c
+puts v
+
+#hash
+
+j = {'Сочинение' => 'все кто сделал' }
+
+#boolean 
+ 
+tri_procenta = true
+konstitucija = false
+
+puts konstitucija
+puts tri_procenta
+
+#range
+
+otmetka = 1..10
+alfavit = 'a'..'z'
+
+#nil
+
+zarplata = nil
+
+
+#здесь меняю типы с помощью to_a
+
+stolb = 1..6
+p stolb.to_a
+
+
+abc = 4
+qwe = 5
+isxod = abc + qwe
+p "abc + qwe = #{isxod}"
+
+
+
+
+
+
+
