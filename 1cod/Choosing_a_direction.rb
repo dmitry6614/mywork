1db560294698e48c88c0c4c071dd1cfda22a2b33
dimka <dmitry_6614@mail.ru>
2024-03-15 20:30:35 +0300

    Choosing_a_direction

--- 1cod/Choosing_a_direction.rb
+++ 1cod/Choosing_a_direction.rb
@@ -1,17 +1,15 @@
 puts 'Выберите своё направление для развития: наука, спорт или искусство'
- user = gets.chomp.to_s
- the_science = 'наука'
- art = 'искусство'
- sport = 'спорт'
+user = gets.chomp.to_s
+the_science = 'наука'
+art = 'искусство'
+sport = 'спорт'
 
- if user == sport
+if user == sport
     puts 'Для достижения в спорте нужно много тренироваться'
- end
-
- if user == art
+elsif
+    user == art
     puts 'Для красивых рисунков нужен талант'
- end
-
- if user == the_science
+elsif
+    user == the_science
     puts 'Нужно читать много книг'
- end
+end
