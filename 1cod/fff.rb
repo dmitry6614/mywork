25052e6e4e8873bf46a176562775e241d725bead
dimka <dmitry_6614@mail.ru>
2024-03-16 17:27:17 +0300

    exchanger

--- 1cod/fff.rb
+++ 1cod/fff.rb
@@ -1,7 +1,18 @@
-puts 'какой курс доллара?'
-kurs = gets.chomp.to_f
-puts "skol'ko otdat' xo4esh?"
-summ = gets.chomp.to_f
-itog = "столько рублей вы получите #{(kurs * summ).round(2)}" 
-puts 'itog'
+puts 'какой курс 1 доллара?'
+dollar_cost = gets.chomp.to_f
 
+puts 'что у вас есть?'
+puts 'USD'
+puts 'BYN'
+user_have = gets.chomp.to_s
+
+puts 'назовите сумму'
+user_summ = gets.chomp.to_f
+
+if  user_have == 'USD'
+    rub = (user_summ * dollar_cost)
+    puts "вы получите #{rub} рублей"
+elsif user_have == 'BYN'
+    dollar = (user_summ / dollar_cost)
+    puts "вы получите #{dollar} долларов"
+end
