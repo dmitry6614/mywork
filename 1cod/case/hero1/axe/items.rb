9fe1a4c0ff1488fddc913b992597b939cab34244
dimka <dmitry_6614@mail.ru>
2024-04-17 23:31:14 +0300

    to be continued 2

---
+++ 1cod/case/hero1/axe/items.rb
@@ -0,0 +1,43 @@
+class Items
+    def initialize(dmg, arm, aps, str, dex, int, hp, mana, price)
+        @dmg = dmg
+        @arm = arm
+        @aps = aps
+        @str = str
+        @dex = dex
+        @int = int
+        @hp = hp
+        @mana = mana
+        @price = price
+    end
+
+    # def calculator(item)
+    #     if @gold >= @price
+
+
+
+    # end
+
+    # def healing_salve
+    #     @hp += 400
+    # end
+    
+    # def consumables
+    #     puts "5.healing salve - #{@price} price"
+    #     puts "6.mango - #{@price} price"
+    #     choice_consumables = STDIN.gets.chomp.to_i
+    #     if choice_consumables == 5
+    #         items.calculator(healing_salve)
+    #     elsif choice_consumables == 6
+    #         items.calculator(mango)
+    #     end
+    # end
+
+    
+    
+    
+    
+    
+end
+
+
--- 1cod/case/hero1/axe/main.rb
+++ 1cod/case/hero1/axe/main.rb
@@ -1,11 +1,12 @@
 require './axe.rb'
+#require './items.rb'
 
 
 axe = Hero.new('AXE', 50, 3.33, 100, 22, 20, 18, 291, 670, 1, 'MELEE', 400, 600, 0)
-
+#items = 
 
 axe.portret
-
+axe.gold_counter
 
 def bar(axe)
    puts "1.farm line         2.farm jungle         3.menu           4.shop"
@@ -24,16 +25,35 @@ def bar(axe)
         puts " Ты убил #{line_exp} крипов  и получил опыта #{line_exp * 60} и #{line_gold * line_exp} gold"
         axe.exp_counter(line_exp * 60)
         bar(axe)
-    elsif unput == 2
-        exp += jungle_exp * 20
-        gold += jungle_exp * jungle_gold
-        puts " Ты убил #{line_exp} крипов  и получил #{jungle_exp * 20} и #{jungle_exp * jungle_gold} gold"
+    elsif input == 2
+        system 'clear' 
+        puts "Ты убил #{jungle_exp} крипов(a) и получил #{jungle_exp * 20} опыта и #{jungle_exp * jungle_gold } голды"
+        axe.exp_counter(jungle_exp * 20)
+        axe.gold_counter(jungle_exp * jungle_gold)
+        bar(axe)       
     elsif input == 3
-        portret
+        system 'clear'
+        puts axe.portret
+        bar(axe)
     elsif input == 4
-        fdfd
+        choice_artefacts(axe)
+        bar(axe)
     else
         puts "Выбери 1, 2, 3 или 4!"
     end
 end
 bar(axe)
+
+def choice_artefacts(axe)
+    puts "1.Consumables      2.Armor       3.Weapons          4.Artifacts       5.menu"
+    buy_artefacts = STDIN.gets.chomp.to_i
+
+    case buy_artefacts
+    when 1
+        consumables
+    when 2
+        puts "7 Assault Cuirass -#{price} price"
+    when 3 
+        puts "9 Daedalus - #{price} price  "
+    end
+end
