a8cd08b9b601cfe2553cc0ae73b8add2c37f4ed7
dimka <dmitry_6614@mail.ru>
2024-05-01 00:00:44 +0300

    new gem

---
+++ 1cod/gems/mail.rb
@@ -0,0 +1,32 @@
+require 'mail'
+require 'io/console'
+
+puts 'Enter sender mail'
+sender_mail = STDIN.gets.chomp
+
+puts 'Enter sender password'
+password = STDIN.noecho(&:gets).chomp
+
+puts 'Enter recipient mail'
+recipient_mail = STDIN.gets.chomp
+
+smtp_settings = {
+    address: "smtp.mail.ru",
+    port: 587,
+    user_name: sender_mail,
+    password: password,
+    authentication: 'plain',
+    enable_starttls_auto: true
+}
+
+mail = Mail.new do
+    from sender_mail
+    to recipient_mail
+    subject "hi!"
+    body "qq bro, you cool man"
+end
+
+mail.delivery_method(:smtp, smtp_settings)
+mail.deliver
+
+puts "Письмо доставлено"
