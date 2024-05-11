b24c351eec3879198cec28251edfb273aa311e4a
dimka <dmitry_6614@mail.ru>
2024-05-11 18:50:51 +0300

    hash 1 lesson

---
+++ 1cod/block2/hashes/hash1.rb
@@ -0,0 +1,16 @@
+ticket = {
+  ticket: "PM 23134432 344",
+  :way => "Moscow",
+  :way2 => "Petushki",
+  :name => "Dmitry",
+  :surname => "Popov",
+  patronymic: "Antonov",
+  passport: "45 99 454637535"
+}
+
+puts "#{ticket[:ticket]}
+#{ticket[:way]} - #{ticket[:way2]}
+#{ticket[:name][0]} #{ticket[:surname]} #{ticket[:patronymic]}
+#{ticket[:passport]}
+"
+
